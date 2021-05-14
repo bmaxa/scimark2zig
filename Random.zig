1452d8af41d8ecd281ee7c53d4986b46463e28e9
Branimir Maksimovic <branimir.maksimovic@gmail.com>
2021-05-14 21:20:27 +0200

    random initizalize moved as member of Random

--- Random.zig
+++ Random.zig
@@ -47,42 +47,42 @@ pub const Random = struct {
     return if (self.haveRange)self.left + dm1 * @intToFloat(f64,k) * self.width
     else dm1 * @intToFloat(f64,k);
   }
+  fn initialize(self: *Random, seed: u32) void{
+    var lseed:u64= seed;
+    var jseed:u64=0;
+    var k0:u64=0;
+    var k1:u64=0;
+    var j0:u64=0;
+    var j1:u64=0;
+    self.seed = seed;
+    if (seed < 0)
+      lseed = -seed;
+    jseed = if (lseed<m1)lseed else m1;
+    if (@rem(jseed,2) == 0) jseed-=1;
+    k0 = 9069 % m2;
+    k1 = 9069 / m2;
+    j0 = @rem(jseed,m2);
+    j1 = @divTrunc(jseed,m2);
+    var iloop:usize = 0;
+    while (iloop<17):(iloop+=1) {
+      jseed = j0 * k0;
+      j1 = @rem((@divTrunc(jseed,m2) + j0 * k1 + j1 * k0),(m2 / 2));
+      j0 = @rem(jseed,m2);
+      self.m[iloop] = @intCast(u32,j0 + m2 * j1);
+    }
+    self.i = 4;
+    self.j = 16;
+  }
 };
 pub fn new(seed:i32,left:f64,right:f64)Random {
   var rc = Random{.m = [_]i32{0} ** 17,.seed = seed, .i = 0, .j = 0, .haveRange = true,
   .left = left, .right = right, .width = right - left};
-  initialize(&rc,seed);
+  rc.initialize(seed);
   return rc;
 }
 pub fn new_seed(seed:u32)Random{
   var rc = Random{.m = [_]u32{0} ** 17,.seed = seed, .i = 0, .j = 0, .haveRange = true,
   .left = 0.0, .right = 1.0, .width = 1.0};
-  initialize(&rc,seed);
+  rc.initialize(seed);
   return rc;
-}
-fn initialize(self: *Random, seed: u32) void{
-  var lseed:u64= seed;
-  var jseed:u64=0;
-  var k0:u64=0;
-  var k1:u64=0;
-  var j0:u64=0;
-  var j1:u64=0;
-  self.seed = seed;
-  if (seed < 0)
-    lseed = -seed;
-  jseed = if (lseed<m1)lseed else m1;
-  if (@rem(jseed,2) == 0) jseed-=1;
-  k0 = 9069 % m2;
-  k1 = 9069 / m2;
-  j0 = @rem(jseed,m2);
-  j1 = @divTrunc(jseed,m2);
-  var iloop:usize = 0;
-  while (iloop<17):(iloop+=1) {
-    jseed = j0 * k0;
-    j1 = @rem((@divTrunc(jseed,m2) + j0 * k1 + j1 * k0),(m2 / 2));
-    j0 = @rem(jseed,m2);
-    self.m[iloop] = @intCast(u32,j0 + m2 * j1);
-  }
-  self.i = 4;
-  self.j = 16;
 }
